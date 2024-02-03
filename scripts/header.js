69423303beedfa2493c2721dc7e60285c9c71d96
TheUltimateKeval <theultimatekeval@gmail.com>
2024-02-03 15:37:59 +0530

    Initialized basic structure for website

---
+++ scripts/header.js
@@ -0,0 +1,14 @@
+headerComponent = document.querySelector("header");
+
+headerComponent.innerHTML = `
+<div class="header-container">
+    <div class="header-logo">Ensemble, IISc</div>
+    <div class="header-nav">
+        <a class="header-nav-item" href="/">Home</a>
+        <a class="header-nav-item" href="/gallery">Gallery</a>
+        <a class="header-nav-item" href="/the-team">The Team</a>
+        <a class="header-nav-item" href="/activites">Activities</a>
+        <a class="header-nav-item" href="/contact">Contact</a>
+    </div>
+</div>
+`;
