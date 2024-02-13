a81e8d35391a8d18cd00f50a4293b5814afcf6b3
Priteh Jogdhankar <priteshjogdhankar@gmail.com>
2024-02-13 12:53:07 +0530

    updated slideshow git

--- scripts/slideshow1.js
+++ scripts/slideshow1.js
@@ -19,7 +19,7 @@ class Slideshow {
       // Looping through the image-caption array to create slides
       this.imgCaptionArray.forEach((slide, index) => {
           let HTMLSlide = document.createElement('div'); // Creating slide container
-          HTMLSlide.classList.add("mySlides"); // Adding class for slide styling
+          HTMLSlide.classList.add(`mySlides${this.id}}`); // Adding class for slide styling
           HTMLSlide.classList.add("fade"); // Adding class for fade effect
           // Setting display style based on index (first slide displayed, others hidden)
           HTMLSlide.setAttribute('style', `${index === 0 ? 'display:block' : 'display:none'}`);
@@ -29,7 +29,7 @@ class Slideshow {
           
           HTMLSlide.innerHTML = `
               <img class="slideShowImg" src="${slide[0]}" style="width:100%">
-              <div class="text">${slide[1]} <span>${index+1}/${this.imgCaptionArray.length}</span></div>
+              <div class="text"><span style="text-align:left;">${slide[1]} </span> <span>${index+1}/${this.imgCaptionArray.length}</span></div>
           `;
           slidesContainer.appendChild(HTMLSlide); // Appending slide to slides container
       });
@@ -39,7 +39,7 @@ class Slideshow {
           let htmlDot = document.createElement('div'); // Creating dot container
           // Setting HTML content for dot with onclick event to navigate to respective slide
           htmlDot.innerHTML = `
-              <span class="${i === 0 ? 'dot active' : 'dot'}" ></span>
+              <span  class="${i === 0 ? `dot SS${this.id}Dots active` : `dot SS${this.id}Dots`}" ></span>
           `;
 
           htmlDot.addEventListener('click', () => this.currentSlide(i + 1));
@@ -48,13 +48,13 @@ class Slideshow {
 
           // Setting HTML content for slideshow navigators (previous and next buttons)
           slideShowNavigators.innerHTML = `
-          <a class="prev">❮</a>
-          <a class="next">❯</a>
+          <a id="prev${this.id}" class="prev">❮</a>
+          <a  id="next${this.id}" class="next">❯</a>
       `;
 
       // Add event listeners for prev and next buttons to navigate slides
-      slideShowNavigators.querySelector('.prev').addEventListener('click', () => this.plusSlides(-1));
-      slideShowNavigators.querySelector('.next').addEventListener('click', () => this.plusSlides(1));
+      slideShowNavigators.querySelector(`#prev${this.id}`).addEventListener('click', () => this.plusSlides(-1));
+      slideShowNavigators.querySelector(`#next${this.id}`).addEventListener('click', () => this.plusSlides(1));
 
 
       // Appending containers to the parent div to display the slideshow
@@ -76,8 +76,8 @@ class Slideshow {
   // Method to display slides and update navigation dots based on current slide index
   showSlides(n) {
       let i;
-      let slides = document.getElementsByClassName("mySlides"); // Get all slides
-      let dots = document.getElementsByClassName("dot"); // Get all dots
+      let slides = document.getElementsByClassName(`mySlides${this.id}}`); // Get all slides
+      let dots = document.getElementsByClassName(`SS${this.id}Dots`); // Get all dots
       if (n > slides.length) { this.slideIndex = 1; } // If index exceeds total slides, reset to 1
       if (n < 1) { this.slideIndex = slides.length; } // If index is less than 1, set to last slide
       // Hide all slides
@@ -94,9 +94,11 @@ class Slideshow {
   }
 }
 
-let homePageSlideShow1ImgArray=[["./img/img1.webp","Maxwell's demon"],["./img/img 2.webp","Maxwell's demon"]]
+let homePageSlideShow1ImgArray=[["./img/img1.webp","Maxwell's demon"],["./img/img 2.webp","<b>Lecture series</b><br> An ongoing lecture talk by Debadrito"],
+["./img/img4.webp","<b>Paper presentation</b><br>After a successfull paper presentation"],["./img/img3.webp","Just a cool poster"]]
 
 let homePageSlideShow1= new Slideshow(homePageSlideShow1ImgArray,1)
+let homePageSlideShow2= new Slideshow(homePageSlideShow1ImgArray,2)
 let homePageSlideShow1HTML=document.getElementById("SS01")
 
 homePageSlideShow1.createHtmlSlideShow(homePageSlideShow1HTML)
