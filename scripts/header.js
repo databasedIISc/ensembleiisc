5d9b3083920931962f2d29de6d9d195db234e47d
TheUltimateKeval <theultimatekeval@gmail.com>
2024-02-22 21:31:25 +0530

    Relative Paths for CSS

--- scripts/header.js
+++ scripts/header.js
@@ -5,11 +5,11 @@ const headerContent= `
 <div class="header-container">
     <div class="header-logo">Ensemble, IISc</div>
     <div class="header-nav">
-        <a class="header-nav-item" href="/">Home</a>
-        <a class="header-nav-item" href="/gallery/">Gallery</a>
-        <a class="header-nav-item" href="/the-team/">The Team</a>
-        <a class="header-nav-item" href="/activities/">Activities</a>
-        <a class="header-nav-item" href="/contact/">Contact</a>
+        <a class="header-nav-item" href="./">Home</a>
+        <a class="header-nav-item" href="./gallery/">Gallery</a>
+        <a class="header-nav-item" href="./the-team/">The Team</a>
+        <a class="header-nav-item" href="./activities/">Activities</a>
+        <a class="header-nav-item" href="./contact/">Contact</a>
     </div>
 </div>
 `;
