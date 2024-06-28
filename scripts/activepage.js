fabaf3a0a7991052e39c03ef90ff6456c600ae8a
Pritesh Jogdhanakar <129420569+pritesh299@users.noreply.github.com>
2024-06-28 17:38:36 +0530

    removed old site

--- scripts/activepage.js
+++
@@ -1,4 +0,0 @@
-console.log(window.location.pathname);
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(`a.header-nav-item[href="${window.location.pathname}"`).classList.add("active");
-});
